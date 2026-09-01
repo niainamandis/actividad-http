7b31d95e3df7237341fa86ece1403ab8babff383
niainamandis <debian@debian>
2026-09-01 10:49:28 -0700

    style: enforce 2-space indentation

--- http_proxy.py
+++ http_proxy.py
@@ -3,213 +3,213 @@ import json
 import sys
  
 def receive_full_message(connection_socket, buff_size):
+  # recibimos la primera parte del mensaje
+  recv_message = connection_socket.recv(buff_size)
+  full_message = recv_message
+  sep = b"\r\n\r\n"
 
-     # recibimos la primera parte del mensaje
+  # verificamos que lleguen los headers
+  while sep not in full_message:
     recv_message = connection_socket.recv(buff_size)
-    full_message = recv_message
-    sep = b"\r\n\r\n"
+    full_message += recv_message
 
-    # verificamos que lleguen los headers
-    while sep not in full_message:
-        recv_message = connection_socket.recv(buff_size)
-        full_message += recv_message
+  headers_raw, body_raw = full_message.split(sep)
+  header_parsed = parse_HTTP_message(headers_raw + sep)
 
-    headers_raw, body_raw = full_message.split(sep)
-    header_parsed = parse_HTTP_message(headers_raw + sep)
+  # verificamos si llegó todo el contenido de body (o si no tiene == 0)
+  if "Content-Length" in header_parsed["headers"]:
+    content_length = int(header_parsed["headers"]["Content-Length"])
+    # entramos a un while para recibir el resto y seguimos esperando información
+    # mientras el buffer no contenga todo el body
+    while len(body_raw) < content_length:
+      # recibimos un nuevo trozo del mensaje
+      recv_message = connection_socket.recv(buff_size)
+      # lo añadimos al mensaje "completo"
+      body_raw += recv_message
+  elif header_parsed["headers"].get("Transfer-Encoding", "") == "chunked":
+    while b"0\r\n\r\n" not in body_raw:
+      # recibimos un nuevo trozo del mensaje
+      recv_message = connection_socket.recv(buff_size)
+      # lo añadimos al mensaje "completo"
+      body_raw += recv_message
 
-    #verificamos si llegó todo el contenido de body (o si no tiene == 0)
-    if "Content-Length" in header_parsed["headers"]:
-      content_length = int(header_parsed["headers"]["Content-Length"])
-       # entramos a un while para recibir el resto y seguimos esperando información
-       # mientras el buffer no contenga todo el body
-      while len(body_raw)<content_length:
-           # recibimos un nuevo trozo del mensaje
-           recv_message = connection_socket.recv(buff_size)
-           # lo añadimos al mensaje "completo"
-           body_raw += recv_message
-    elif header_parsed["headers"].get("Transfer-Encoding", "") == "chunked":
-      while b"0\r\n\r\n" not in body_raw:
-           # recibimos un nuevo trozo del mensaje
-           recv_message = connection_socket.recv(buff_size)
-           # lo añadimos al mensaje "completo"
-           body_raw += recv_message
-
-     # finalmente retornamos el mensaje
-    return headers_raw + sep + body_raw
+  # finalmente retornamos el mensaje
+  return headers_raw + sep + body_raw
 
  
 def parse_HTTP_message(http_message: bytes):
-    # separar header del body
-    # nota: agregar b"" => busca en bytes :D
-    headers_cod, body = http_message.split(b"\r\n\r\n", 1)
-    # decodificar las lineas del header
-    headers_text = headers_cod.decode()
-    lines = headers_text.split("\r\n")
-    # guardar el header en un diccionario
-    headers = {}
-    for line in lines[1:]: #ignoramos lines[0] pke es solo get y http
-        if not line: continue
-        key, val = line.split(": ", 1)
-        headers[key] = val
-    # juntarlo con el body y retornar 
-    return {
-            "f_line": lines[0],
-            "headers": headers,
-            "body": body
-            }
+  # separar header del body
+  # nota: agregar b"" => busca en bytes :D
+  headers_cod, body = http_message.split(b"\r\n\r\n", 1)
+  # decodificar las lineas del header
+  headers_text = headers_cod.decode()
+  lines = headers_text.split("\r\n")
+  # guardar el header en un diccionario
+  headers = {}
+  for line in lines[1:]: # ignoramos lines[0] pke es solo get y http
+    if not line: continue
+    key, val = line.split(": ", 1)
+    headers[key] = val
+  # juntarlo con el body y retornar 
+  return {
+    "f_line": lines[0],
+    "headers": headers,
+    "body": body
+  }
  
 
 # NOTE: debería funcionar pke en ningún momento decodificamos
 # body en parse,,, entonces body debería seguir en bytes
 def create_HTTP_message(parsed_msg: dict):
-    #reconstruir el msje http
-    msg_text = parsed_msg["f_line"] + "\r\n"
-    for key, val in parsed_msg["headers"].items():
-        msg_text += f"{key}" + ": " + f"{val}" + "\r\n"
-    #fin headers "\r\n\r\n"
-    msg_text += "\r\n"
-    #codificarlo
-    header_bytes = msg_text.encode()
-    return header_bytes + parsed_msg["body"]
+  # reconstruir el msje http
+  msg_text = parsed_msg["f_line"] + "\r\n"
+  for key, val in parsed_msg["headers"].items():
+    msg_text += f"{key}: {val}\r\n"
+  # fin headers "\r\n\r\n"
+  msg_text += "\r\n"
+  # codificarlo
+  header_bytes = msg_text.encode()
+  return header_bytes + parsed_msg["body"]
 
 if __name__ == "__main__":
+  # para leer el nombre o ruta del archivo json
+  config_path = sys.argv[1]
+  with open(config_path) as file:
+    data = json.load(file)
+  
+  username = data.get("user", "usuario_desconocido")
+  blocked_url = data.get("blocked", [])
+  forbidden_words = data.get("forbidden_words", [])
+  image_root = "/cat.jpg"
 
-    # para leer el nombre o ruta del archivo json
-     config_path = sys.argv[1]
-     with open(config_path) as file:
-        data = json.load(file)
-     username = data.get("user", "usuario_desconocido")
-     blocked_url = data.get("blocked", [])
-     image_root = "/cat.jpg"
+  print(f"server iniciado para el usuario: {username} :3")
 
-     print(f"server iniciado para el usuario: {username} :3")
-
-     # definimos el tamaño del buffer de recepción y la secuencia de fin de mensaje
-     buff_size = 4
-     IP_VM = "192.168.64.2"
-     listen_socket_address = (IP_VM, 8000)
-     
-     print("Creando socket de escucha - Proxy")
-     # creamos un socket orientado a conexión
-     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     # le indicamos al socket de escucha que debe atender peticiones en la dirección address
-     listen_socket.bind(listen_socket_address)
-     # definimos que puede tener hasta 3 peticiones de conexión encoladas
-     listen_socket.listen(3)
-     
-     # nos quedamos esperando a que llegue una petición de conexión
-     print(f"... Esperando clientes en http://{IP_VM}:8000")
-     while True:
-        # cuando llega una petición de conexión la aceptamos
-        # y se crea un nuevo socket que se comunicará con el cliente
-        client_socket, client_socket_address = listen_socket.accept()
+  # definimos el tamaño del buffer de recepción y la secuencia de fin de mensaje
+  buff_size = 4
+  IP_VM = "192.168.64.2"
+  listen_socket_address = (IP_VM, 8000)
+  
+  print("Creando socket de escucha - Proxy")
+  # creamos un socket orientado a conexión
+  listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  # le indicamos al socket de escucha que debe atender peticiones en la dirección address
+  listen_socket.bind(listen_socket_address)
+  # definimos que puede tener hasta 3 peticiones de conexión encoladas
+  listen_socket.listen(3)
+  
+  # nos quedamos esperando a que llegue una petición de conexión
+  print(f"... Esperando clientes en http://{IP_VM}:8000")
+  while True:
+    # cuando llega una petición de conexión la aceptamos
+    # y se crea un nuevo socket que se comunicará con el cliente
+    client_socket, client_socket_address = listen_socket.accept()
  
-        # recibimos y parseamos la request del cliente
-        recv_message = receive_full_message(client_socket, buff_size)
-        parsed_req = parse_HTTP_message(recv_message)
-        print(f"-> Se ha recibido la siguiente petición: {parsed_req["f_line"]}")
-         
-        # revisamos si la página está bloqueada
-        url_req = parsed_req["f_line"].split(" ")[1]
-        url_req = url_req.removeprefix("http://")
-        is_blocked = False
-        for url in blocked_url:
-          if url == url_req:
-            is_blocked = True
+    # recibimos y parseamos la request del cliente
+    recv_message = receive_full_message(client_socket, buff_size)
+    parsed_req = parse_HTTP_message(recv_message)
+    print(f"-> Se ha recibido la siguiente petición: {parsed_req['f_line']}")
+      
+    # revisamos si la página está bloqueada
+    url_req = parsed_req["f_line"].split(" ")[1]
+    url_req = url_req.removeprefix("http://").removeprefix("https://")
+    is_blocked = False
+    
+    for url in blocked_url:
+      if url_req.startswith(url):
+        is_blocked = True
 
-        # revisamos si la request es por la imagen local
-        is_image_request = url_req.endswith(image_root)
+    # revisamos si la request es por la imagen local
+    is_image_request = url_req.endswith(image_root)
 
-        # si la request es por la imagen
-        if is_image_request: 
-           print(f"Mostrando imagen local {image_root}")
+    # si la request es por la imagen
+    if is_image_request: 
+      print(f"Mostrando imagen local {image_root}")
 
-           with open("cat.jpg", "rb") as f:
-              img_body = f.read()
+      with open("cat.jpg", "rb") as f:
+        img_body = f.read()
 
-           response = {
-                "f_line": "HTTP/1.1 200 OK",
-                "headers": {
-                    "Content-Type": "image/jpeg",
-                    "Content-Length": str(len(img_body)),
-                    "Connection": "keep-alive",
-                    "Access-Control-Allow-Origin": "*",
-                    },
-                "body": img_body
-           }
+      response = {
+        "f_line": "HTTP/1.1 200 OK",
+        "headers": {
+          "Content-Type": "image/jpeg",
+          "Content-Length": str(len(img_body)),
+          "Connection": "keep-alive",
+          "Access-Control-Allow-Origin": "*",
+        },
+        "body": img_body
+      }
 
-           server_ans = create_HTTP_message(response)
+      server_ans = create_HTTP_message(response)
 
-        # si está bloqueada 
-        elif is_blocked:
-           print(f"La página {url_req} está bloqueada")
+    # si está bloqueada 
+    elif is_blocked:
+      print(f"La página {url_req} está bloqueada")
 
-           # enviar el error 
-           html_body = """<!DOCTYPE html> 
-                          <html lang="es">
-                            <head><meta charset="UTF-8">
-                              <title>CC4303</title>
-                            </head>
-                            <body>
-                              <h1>Página bloqueada</h1>
-                              <img src="/cat.jpg" alt="gato">
-                            </body>
-                          </html>""".encode()
+      # enviar el error 
+      html_body = """<!DOCTYPE html> 
+        <html lang="es">
+          <head><meta charset="UTF-8">
+            <title>CC4303</title>
+          </head>
+          <body>
+            <h1>Página bloqueada</h1>
+            <img src="/cat.jpg" alt="gato">
+          </body>
+        </html>""".encode()
 
-           # respondemos un msje http
-           response = {
-                "f_line": "HTTP/1.1 403 Forbidden",
-                "headers": {
-                    "Content-Type": "text/html; charset=utf-8",
-                    "Content-Length": str(len(html_body)),
-                    "Connection": "keep-alive",
-                    "Access-Control-Allow-Origin": "*",
-                    "X-ElQuePregunta": username
-                    },
-                "body": html_body
-           }
+      # respondemos un msje http
+      response = {
+        "f_line": "HTTP/1.1 403 Forbidden",
+        "headers": {
+          "Content-Type": "text/html; charset=utf-8",
+          "Content-Length": str(len(html_body)),
+          "Connection": "keep-alive",
+          "Access-Control-Allow-Origin": "*",
+          "X-ElQuePregunta": username
+        },
+        "body": html_body
+      }
  
-           # el mensaje debe pasarse a bytes antes de ser enviado
-           server_ans = create_HTTP_message(response)
+      # el mensaje debe pasarse a bytes antes de ser enviado
+      server_ans = create_HTTP_message(response)
 
-        # sino 
-        else:
-          # enviar el request al servidor
+    # sino 
+    else:
+      # enviar el request al servidor
 
-          # extraer el host de la request del cliente 
-          server_domain = parsed_req["headers"]["Host"]
-          server_host = server_domain.split(":", 1)[0]
-          server_port = int(server_domain.split(":", 1)[1]) if ":" in server_domain else 80
+      # extraer el host de la request del cliente 
+      server_domain = parsed_req["headers"]["Host"]
+      server_host = server_domain.split(":", 1)[0]
+      server_port = int(server_domain.split(":", 1)[1]) if ":" in server_domain else 80
 
-          # crear un socket para el servidor y conectarse 
-          server_socket_address = (server_host, server_port)
-          server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          print(f"-> Conectando a {server_host}:{server_port}")
-          server_socket.connect(server_socket_address)
-          print("Conexión con el servidor establecida")
-          
-          # hay que añadir el header de quien pregunta 
-          parsed_req["headers"]["X-ElQuePregunta"] = username
+      # crear un socket para el servidor y conectarse 
+      server_socket_address = (server_host, server_port)
+      server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      print(f"-> Conectando a {server_host}:{server_port}")
+      server_socket.connect(server_socket_address)
+      print("Conexión con el servidor establecida")
+      
+      # hay que añadir el header de quien pregunta 
+      parsed_req["headers"]["X-ElQuePregunta"] = username
 
-          # pasamos la request a bytes para poder enviarla al servidor
-          modified_request = create_HTTP_message(parsed_req)
+      # pasamos la request a bytes para poder enviarla al servidor
+      modified_request = create_HTTP_message(parsed_req)
 
-          # enviar la request al servidor
-          server_socket.send(modified_request)
+      # enviar la request al servidor
+      server_socket.send(modified_request)
 
-          # recibir respuesta del servidor 
-          server_ans = receive_full_message(server_socket, buff_size)
-          server_parsed_req = parse_HTTP_message(server_ans)
-          print(f"-> Respuesta recibida del servidor: {len(server_ans)} bytes")
+      # recibir respuesta del servidor 
+      server_ans = receive_full_message(server_socket, buff_size)
+      server_parsed_req = parse_HTTP_message(server_ans)
+      print(f"-> Respuesta recibida del servidor: {len(server_ans)} bytes")
 
-          # cerrar la conexión al servidor
-          server_socket.close()
-          print(f"Conexión con {server_host}:{server_port} ha sido cerrada")
+      # cerrar la conexión al servidor
+      server_socket.close()
+      print(f"Conexión con {server_host}:{server_port} ha sido cerrada")
 
-        # reenviar respuesta al cliente 
-        client_socket.send(server_ans)
+    # reenviar respuesta al cliente 
+    client_socket.send(server_ans)
 
-        # cerramos la conexión con el cliente
-        client_socket.close()
-        print(f"Conexión con {client_socket_address} ha sido cerrada")
- 
+    # cerramos la conexión con el cliente
+    client_socket.close()
+    print(f"Conexión con {client_socket_address} ha sido cerrada")
